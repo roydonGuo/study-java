360364ba60eb806c35c45322b671ed718c9c8750
roydonGuo <3133010060@qq.com>
2023-09-23 22:41:17 +0800

    walkFileTree demo

---
+++ netty/src/main/java/com/roydon/section_01/WalkFileTree.java
@@ -0,0 +1,63 @@
+package com.roydon.section_01;
+
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * WalkFileTree
+ *
+ * @AUTHOR: roydon
+ * @DATE: 2023/9/23
+ * 遍历文件树
+ **/
+public class WalkFileTree {
+    public static void main(String[] args) throws IOException {
+
+        AtomicInteger dirCount = new AtomicInteger();
+        AtomicInteger fileCount = new AtomicInteger();
+
+        Files.walkFileTree(Paths.get("E:\\baidu\\BaiduNetdiskDownload\\Netty网络编程\\Netty教程源码资料"), new SimpleFileVisitor<Path>() {
+            /**
+             * Invoked for a directory before entries in the directory are visited.
+             *
+             * <p> Unless overridden, this method returns {@link FileVisitResult#CONTINUE
+             * CONTINUE}.
+             *
+             * @param dir
+             * @param attrs
+             */
+            @Override
+            public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+                System.out.println("dir =====> " + dir);
+                dirCount.incrementAndGet();
+                // 若是删除文件夹需要确保文件夹下内容为空否则报错
+                return super.preVisitDirectory(dir, attrs);
+            }
+
+            /**
+             * Invoked for a file in a directory.
+             *
+             * <p> Unless overridden, this method returns {@link FileVisitResult#CONTINUE
+             * CONTINUE}.
+             *
+             * @param file
+             * @param attrs
+             */
+            @Override
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                System.out.println("file = " + file);
+                fileCount.incrementAndGet();
+                // 统计md文档的数量
+                if (file.toFile().getName().endsWith("nio.md")) {
+//                    fileCount.incrementAndGet();
+//                    Files.delete(file);
+                }
+                return super.visitFile(file, attrs);
+            }
+        });
+        System.out.println(dirCount);
+        System.out.println(fileCount);
+    }
+}
---
+++ netty/src/main/java/com/roydon/section_01/WalkFileTreeCopy.java
@@ -0,0 +1,37 @@
+package com.roydon.section_01;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+/**
+ * FileTreeCopy
+ *
+ * @AUTHOR: roydon
+ * @DATE: 2023/9/23
+ * copy多级目录
+ **/
+public class WalkFileTreeCopy {
+    public static void main(String[] args) throws IOException {
+        long start = System.currentTimeMillis();
+        String source = "E:\\baidu\\BaiduNetdiskDownload\\2、黑马程序员Java项目《学成在线》企业级开发实战\\学成在线项目—视频\\day01 项目介绍&环境搭建";
+        String target = "E:\\baidu\\BaiduNetdiskDownload\\2、黑马程序员Java项目《学成在线》企业级开发实战\\学成在线项目—视频\\day01 项目介绍&环境搭建-copy";
+        Files.walk(Paths.get(source)).forEach(path -> {
+            try {
+                String targetName = path.toString().replace(source, target);
+                // 是目录
+                if (Files.isDirectory(path)) {
+                    Files.createDirectory(Paths.get(targetName));
+                }
+                // 是普通文件
+                else if (Files.isRegularFile(path)) {
+                    Files.copy(path, Paths.get(targetName));
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+        long end = System.currentTimeMillis();
+        System.out.println(end - start);
+    }
+}
